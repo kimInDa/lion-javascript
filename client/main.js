faea0540924780e839542dd241dd7e877334cd30
kimInDa <pinktail2327@naver.com>
2023-07-27 15:47:29 +0900

    17일차 수업 - 주석 추가

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { deleteStorage, getNode, getStorage, setStorage } from './lib/index.js'
 
 // 로컬스토리지를 활용하여 새로고침 되어도 텍스트필드에 입력한 글자가 계속 남아있을 수 있도록 하기
+// clear 버튼 누르면 지워지는 기능은 각자 만들어보기
 
 const textField = getNode('#textField');
 const clearButton = getNode('button');
